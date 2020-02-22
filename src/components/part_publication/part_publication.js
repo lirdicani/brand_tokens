01d849834d441f94b6721ecb50006ee2317194fe
lirdi <cani.lirdi@gmail.com>
2020-02-23 06:26:37 +0800

    Landing page 2/22/2020-new

--- src/components/part_publication/part_publication.js
+++ src/components/part_publication/part_publication.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 import './part_publication.css';
-import BlogCardModule from './blog_card_module/blog_card_module';
+// import BlogCardModule from './blog_card_module/blog_card_module';
 
 const PartPublication = () => {
     return (
@@ -13,7 +13,7 @@ const PartPublication = () => {
                 <div className='part-publication-blog-title-intro'>From our blog</div>
                 <Button className='more-blog' >SEE MORE FROM OUR BLOG</Button>
             </div>
-            <BlogCardModule />
+            {/* <BlogCardModule /> */}
         </div>
     )
 }
